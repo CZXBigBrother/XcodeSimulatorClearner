4568a732409405659c40f00528159c70b5bfa7f5
“marcoChen” <mchenxing@hotmail.com>
2016-11-30 10:57:34 +0800

    修改版本

--- XcodeSimulatorClearner.py
+++ XcodeSimulatorClearner.py
@@ -5,6 +5,7 @@ __author__ = 'chenzhengxing'
 import os
 import shutil
 import time
+import getpass
 
 computerName = ''
 path = []
@@ -13,16 +14,16 @@ def getPath():
 
 def searchFile(files,path):
     for x in  os.listdir(path):
+        print x
         if x != '.DS_Store':
             files.append(path + x)
-    return files
 
 def isExistsPath():
     return os.path.exists(getPath())
 
 def setPath():
     files = []
-    path = ['/Users/{0}/Library/Developer/Xcode/DerivedData/'.format(computerName),'/Users/{0}/Library/Developer/Xcode/Archives/'.format(computerName)]
+    path = ['/Users/{0}/Library/Developer/Xcode/DerivedData/'.format(computerName)]#,'/Users/{0}/Library/Developer/Xcode/Archives/DerivedData/'.format(computerName)
     for apath in path:
         searchFile(files,apath)
     return files
@@ -31,50 +32,26 @@ def deleteFile():
     startTime = time.time()
     files = setPath();
     for afile in files:
+        print afile
         shutil.rmtree(afile,True)
         print '删除:' + afile
     print('删除完毕...耗时:'+ str(time.time() - startTime))
     print('共删除{0}个编译文件'.format(len(files)))
 
-def getCurrentPath():
-    return os.path.abspath('.')
-
-def getConfig():
-    if os.path.exists(getCurrentPath() + '/config.txt'):
-        f = open(getCurrentPath() + '/config.txt', 'r')
-        return f.read()
-    return ''
-
-def writeConfig(user):
-    f = open(getCurrentPath() + '/config.txt', 'w')
-    f.write(user)
-    f.close()
 if __name__ == '__main__':
     isQuit = True
-    computerName = 'cheng'
-    if getConfig():
-        print '是否使用:{0}(y/n)'.format(getConfig())
-        if raw_input().lower() == 'y':
-            computerName = getConfig()
-        else:
-            print('请输入您的计算机名称:')
-            computerName = raw_input()
-    else:
-        print('请输入您的计算机名称:')
-        computerName = raw_input()
+    computerName = getpass.getuser()
+    print getpass.getuser()
     while isQuit:
         print('检查路径是否存在...')
         if isExistsPath():
-            print('检查成功,是否开始删除(y/n):')
+            print('检查成功,是否开始清楚编译缓存(y/n):')
             sure = raw_input().lower()
             if sure == 'y':
-                writeConfig(computerName)
                 deleteFile()
                 isQuit = False
             else:
                 isQuit = False
         else:
-            print('路径错误或者用户名输入错误,或按n退出程序')
-            computerName = raw_input().lower()
-            if computerName == 'n':
-                isQuit = False
+            print('路径不存在，按n退出程序')
+            isQuit = False
